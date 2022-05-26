902a47fddad7271ef330102d70068db95a267e8e
Israel <igarashiisrael@icloud.com>
2022-05-26 19:04:40 -0300

    Withdraw method tests implemented.

--- Aula-JUnit/aula-junit/src/tests/entities/AccountTests.java
+++ Aula-JUnit/aula-junit/src/tests/entities/AccountTests.java
@@ -33,13 +33,31 @@ public class AccountTests {
     public void fullWithdrawShouldClearBalanceAndReturnFullBalance(){
 
         double expectedValue = 0.0;
-        double initialBalance = 800.00;
+        double initialBalance = 800.0;
         Account acc = AccountFactory.createAccount(initialBalance);
 
         double result = acc.fullWithdraw();
 
         Assertions.assertTrue(expectedValue == acc.getBalance());
         Assertions.assertTrue(result == initialBalance);
+    }
 
+    @Test
+    public void withdrawShouldDecreaseBalanceWhenSuficientBalance(){
+
+        Account acc = AccountFactory.createAccount(800.0);
+
+        acc.withdraw(500.0);
+
+        Assertions.assertEquals(300.0, acc.getBalance());
+    }
+
+    @Test
+    public void withdrawShouldThrowExceptionWhenInsufficientBalance(){
+
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            Account acc = AccountFactory.createAccount(800.0);
+            acc.withdraw(801.0);
+        });
     }
 }
