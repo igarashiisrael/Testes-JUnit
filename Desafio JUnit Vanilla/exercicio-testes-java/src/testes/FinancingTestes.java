d97e105d7422b50c6f6ae3106157accb555687d5
Israel <igarashiisrael@icloud.com>
2022-05-31 14:04:49 -0300

    Desafio JUnit Vanilla

---
+++ Desafio JUnit Vanilla/exercicio-testes-java/src/testes/FinancingTestes.java
@@ -0,0 +1,118 @@
+package testes;
+
+import entities.Financing;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+public class FinancingTestes {
+
+    //arrange
+    @Test
+    public void constructorShouldCreateObjectWhenValidData() {
+
+
+        //action
+        Financing f = new Financing(100000.0, 2000.0, 80);
+
+        //assert
+        Assertions.assertEquals(100000.0, f.getTotalAmount());
+        Assertions.assertEquals(2000.0, f.getIncome());
+        Assertions.assertEquals(80, f.getMonths());
+    }
+
+    @Test
+    public void constructorShouldThrowIllegalArgumentExceptionWhenInvalidData() {
+
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            Financing f = new Financing(100000.0, 2000.0, 20);
+        });
+    }
+
+
+    @Test
+    public void setTotalAmountShouldSetDataWhenValidData() {
+
+        //arrange
+        Financing f = new Financing(100000.0, 2000.0, 80);
+
+        //action
+        f.setTotalAmount(90000.0);
+
+        //assert
+        Assertions.assertEquals(90000.0, f.getTotalAmount());
+    }
+
+    @Test
+    public void setTotalAmountShouldThrowIllegalArgumentExceptionWhenInvalidData() {
+
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            Financing f = new Financing(100000.0, 2000.0, 80);
+            f.setTotalAmount(110000.0);
+        });
+    }
+
+
+    @Test
+    public void setIncomeShouldSetDataWhenValidData() {
+
+        //arrange
+        Financing f = new Financing(100000.0, 2000.0, 80);
+
+        //action
+        f.setIncome(2100.0);
+
+        //assert
+        Assertions.assertEquals(2100.0, f.getIncome());
+    }
+
+    @Test
+    public void setTIncomeShouldThrowIllegalArgumentExceptionWhenInvalidData() {
+
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            Financing f = new Financing(100000.0, 2000.0, 80);
+            f.setIncome(1900.0);
+        });
+    }
+
+    @Test
+    public void setMonthsShouldSetDataWhenValidData() {
+
+        //arrange
+        Financing f = new Financing(100000.0, 2000.0, 80);
+
+        //action
+        f.setMonths(81);
+
+        //assert
+        Assertions.assertEquals(81, f.getMonths());
+    }
+
+    @Test
+    public void setTMonthsShouldThrowIllegalArgumentExceptionWhenInvalidData() {
+
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            Financing f = new Financing(100000.0, 2000.0, 80);
+            f.setMonths(79);
+        });
+    }
+
+    @Test
+    public void entryShouldCalculateEntryCorrectly(){
+
+        Financing f = new Financing(100000.0, 2000.0, 80);
+
+        Assertions.assertEquals(20000.0, f.entry());
+    }
+
+    @Test
+    public void quotaShouldCalculateQuotaCorrectly(){
+
+        Financing f = new Financing(100000.0, 2000.0, 80);
+
+        Assertions.assertEquals(1000.0, f.quota());
+    }
+
+
+
+
+}
+
